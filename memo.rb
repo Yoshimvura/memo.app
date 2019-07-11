6d28e976df32d4ca764dc6bb99e4f65c18d85f33
yoshimura <kyosimura23@gmail.com>
2019-07-12 00:54:39 +0900

    change url & add file lock

--- memo.rb
+++ memo.rb
@@ -5,26 +5,14 @@ require 'sinatra/reloader'
 require 'erb'
 require 'csv'
 require 'slim'
-enable :method_override
 
-get '/' do
+get '/memos' do
   Memo.organize
   @table = CSV.table('csv/memo.csv', headers: %w[id title body])
   slim :top
 end
 
-post '/' do
-  Memo.create(title: params['title'], body: params['body'])
-  redirect to('/')
-end
-
-patch '/edit/update/:id' do |id|
-  Memo.update(id, params['title'], params['body'])
-  Memo.organize
-  redirect to('/')
-end
-
-get '/edit/:id' do |id|
+get '/memos/:id/edit' do |id|
   id = id.to_i
   @title = Memo.title(id)
   @body = Memo.body(id)
@@ -32,22 +20,33 @@ get '/edit/:id' do |id|
   slim :edit
 end
 
-get '/new' do
+get '/memos/new' do
   @title = '新規登録'
   @content = 'memo new'
   slim :new
 end
 
-get '/show/:id' do |id|
+get '/memos/:id' do |id|
   @id = id.to_i
   @title = Memo.title(id)
   @body = Memo.body(id)
   slim :show
 end
 
-delete '/show/delete/:id' do |id|
+post '/memos' do
+  Memo.create(title: params['title'], body: params['body'])
+  redirect to('/memos')
+end
+
+patch '/memos/:id' do |id|
+  Memo.update(id, params['title'], params['body'])
+  Memo.organize
+  redirect to('/memos')
+end
+
+delete '/memos/:id' do |id|
   Memo.delete(id)
-  redirect to('/')
+  redirect to('/memos')
 end
 
 class Memo
@@ -67,28 +66,31 @@ class Memo
       @table = CSV.table('csv/memo.csv', headers: %w[id title body])
       @new_id_num = @table.size
       if @table.first.nil?
-        CSV.open('csv/memo.csv', 'w', headers: true) do |test|
-          test << %w[id title body]
-          test << [1, title, body]
+        CSV.open('csv/memo.csv', 'w', headers: true) do |file|
+          file.flock(File::LOCK_EX)
+          file << %w[id title body]
+          file << [1, title, body]
         end
 
       else
-        CSV.open('csv/memo.csv', 'a', headers: true) do |test|
-          test << [@new_id_num, title, body]
+        CSV.open('csv/memo.csv', 'a', headers: true) do |file|
+          file.flock(File::LOCK_EX)
+          file << [@new_id_num, title, body]
         end
       end
     end
 
     def organize
       @table = CSV.table('csv/memo.csv', headers: %w[id title body])
-      CSV.open('csv/memo.csv', 'w', headers: true) do |csv|
+      CSV.open('csv/memo.csv', 'w', headers: true) do |file|
+        file.flock(File::LOCK_EX)
         i = 0
         @table.each do |f|
           reg = Regexp.new(/\d/)
           f[0] = (i += 1) if reg.match((f[0]).to_s)
           next if f[0].nil?
 
-          csv.puts f
+          file.puts f
         end
       end
     end
@@ -96,9 +98,10 @@ class Memo
     def delete(id)
       @table.by_row
       @table.delete_if { |f| f[:id] == id.to_i }
-      CSV.open('csv/memo.csv', 'w', headers: true) do |csv|
+      CSV.open('csv/memo.csv', 'w', headers: true) do |file|
+        file.flock(File::LOCK_EX)
         @table.each do |row|
-          csv.puts row
+          file.puts row
         end
       end
     end
@@ -112,9 +115,10 @@ class Memo
           f[2] = body
         end
       end
-      CSV.open('csv/memo.csv', 'w', headers: true) do |csv|
+      CSV.open('csv/memo.csv', 'w', headers: true) do |file|
+        file.flock(File::LOCK_EX)
         @table.each do |row|
-          csv.puts row
+          file.puts row
         end
       end
     end
